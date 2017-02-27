6f7eace4d8368b8734f1fbb35aab2a520e7004c5
Ken Sipe <kensipe@gmail.com>
2017-02-27 16:26:10 -0600

    matching all scale numbers and up to 50k

--- tests/scale/test_root_marathon_scale.py
+++ tests/scale/test_root_marathon_scale.py
@@ -30,7 +30,7 @@ test_log = []
   (1, 5000),
   (1, 10000),
   (1, 25000),
-  (1, 47000)
+  (1, 50000)
 ])
 def test_instance_scale(num_apps, num_instances):
     """ Runs scale tests on `num_instances` of usually 1 app.
@@ -52,7 +52,8 @@ def test_instance_scale(num_apps, num_instances):
   (1000, 1),
   (5000, 1),
   (10000, 1),
-  (25000, 1)
+  (25000, 1),
+  (50000, 1)  
 ])
 def test_count_scale(num_apps, num_instances):
     """ Runs scale test on `num_apps` usually 1 instance each.
@@ -74,7 +75,9 @@ def test_count_scale(num_apps, num_instances):
   (500, 1),
   (1000, 1),
   (5000, 1),
-  (10000, 1)
+  (10000, 1),
+  (25000, 1),
+  (50000, 1)
 ])
 def test_group_scale(num_apps, num_instances):
     """ Runs scale test on `num_apps` usually 1 instance each deploy as a group.
