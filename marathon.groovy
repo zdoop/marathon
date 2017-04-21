be293b983f65ea1ea5d207b2cd7b8ac8008c5e2e
Jason Gilanfarr <jgilanfarr@mesosphere.com>
2017-04-21 16:39:52 -0700

    Report Github considering our unstable status and report to slack with two messages (one if unstable fails, one for success even if unstable fails
    Summary: - This actually makes us correctly green
    
    Test Plan: pipelines/*
    
    Reviewers: timcharper, jeschkies, aquamatthias, jenkins
    
    Subscribers: marathon-dev
    
    Differential Revision: https://phabricator.mesosphere.com/D721

--- marathon.groovy
+++ marathon.groovy
@@ -160,7 +160,7 @@ def report_success() {
     }
   } else {
     if (is_master_or_release()) {
-      if (previousBuildFailed() && currentBuild.result == 'SUCCESS') {
+      if (previousBuildFailed()) {
         slackSend(
             message: "\u2714 ̑̑branch `${env.BRANCH_NAME}` is green again. (<${env.BUILD_URL}|Open>)",
             color: "good",
@@ -171,6 +171,14 @@ def report_success() {
     step([$class: 'GitHubCommitStatusSetter'
         , errorHandlers: [[$class: 'ShallowAnyErrorHandler']]
         , contextSource: [$class: 'ManuallyEnteredCommitContextSource', context: "Velocity All"]
+        , statusResultSource: [
+            $class: 'ConditionalStatusResultSource'
+            , results: [
+                [$class: 'BetterThanOrEqualBuildResult', result: 'UNSTABLE', state: 'SUCCESS', message: build.description],
+                [$class: 'BetterThanOrEqualBuildResult', result: 'FAILURE', state: 'FAILURE', message: build.description],
+                [$class: 'AnyBuildResult', state: 'FAILURE', message: 'Loophole']
+            ]
+        ]
     ])
   }
 }
